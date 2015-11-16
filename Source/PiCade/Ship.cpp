d90dfaa137709015cee7e19d36e9cd42e6ab680f
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-16 14:00:23 +0000

    all firing of ships is now controlled in the Ship class

--- Source/PiCade/Ship.cpp
+++ Source/PiCade/Ship.cpp
@@ -1,1 +1,77 @@
 #include "Ship.h"
+
+Ship::Ship(float movementSpeed, int firingSpeed, int movementDirection, irr::ITimer *timerReference, const irr::io::path& pathOfMesh, const irr::io::path& pathOfTexture, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference, bool spawnOnConstruct)
+        : Object(pathOfMesh, pathOfTexture, sceneManagerReference, driverReference, spawnOnConstruct){
+    
+    //set up variables
+    moveSpeed = movementSpeed;
+    turnSpeed = moveSpeed + 10.0f;
+    //direction of movement +1 is along the positive X axis, -1 is along the negative X axis
+    moveDir = movementDirection;
+    
+    //time between each bullet firing 1000 = 1 second
+    timeBetweenShots = firingSpeed;
+    timeSinceLastFire = 0;
+    canFire = true;
+    this->timerReference = timerReference;
+    
+    //reference the pointers
+    smgr = sceneManagerReference;
+    drv = driverReference;
+}
+
+Ship::~Ship(){
+    //clear references (these get deleted properly elsewhere)
+    smgr = 0;
+    drv = 0;
+    
+    //delete the pointers
+    delete smgr;
+    delete drv;
+    delete bullet;
+}
+
+void Ship::tick(irr::f32 deltaTime){
+    //iterate through the list of fired bullets and update them
+    for(std::list<Bullet*>::iterator bulletIterator = firedBullets.begin(); bulletIterator != firedBullets.end(); ++bulletIterator){
+        if((*bulletIterator)->checkLifeTime()){
+            //store the current iterator object
+            Bullet *toDelete = *bulletIterator;
+            //erase the object from the list
+            bulletIterator = firedBullets.erase(bulletIterator);
+            //delete the data stored in memory and invoke the destructor
+            delete toDelete;
+        }else{
+            //call update function
+            (*bulletIterator)->tick(deltaTime);
+        }
+    }
+    
+    //check how long is left before the ship can fire again
+    if(!canFire){
+        if((timeSinceLastFire + timeBetweenShots) < timerReference->getRealTime()){
+            canFire = true;
+        }
+    }
+}
+
+void Ship::shoot(){
+    if(canFire){
+        //construct a new bullet
+        bullet = new Bullet(smgr, drv);
+        //fire it
+        bullet->fire(objectPosition);
+        
+        //add it onto the list to be updated
+        firedBullets.push_back(bullet);
+        
+        //clear the pointer to prevent memory leaks
+        bullet = 0;
+        
+        //stop the ship firing immediately after
+        canFire = false;
+        
+        //update the time since last fire variables
+        timeSinceLastFire = timerReference->getRealTime();
+    }
+}
--- Source/PiCade/Ship.h
+++ Source/PiCade/Ship.h
@@ -6,7 +6,51 @@
 #ifndef SHIP_H
 #define	SHIP_H
 
+#include <list>
 
+#include "Object.h"
+#include "Bullet.h"
+
+class Ship : public Object{
+    //VARIABLES
+protected:
+    //how fast the ship can move
+    float moveSpeed;
+    //how quickly the ship will move up/down/left/right
+    float turnSpeed;
+    //the direction to move in (will be +1 or -1)
+    short moveDir;
+    
+private:
+    //if the ship is able to fire
+    bool canFire;
+    //control the time between each bullet firing
+    irr::u32 timeSinceLastFire;
+    irr::ITimer *timerReference;
+    int timeBetweenShots;
+    
+    //the bullets the ship will fire
+    Bullet *bullet;
+    std::list<Bullet*> firedBullets;
+    
+    //needed to construct new bullets
+    irr::scene::ISceneManager *smgr;
+    irr::video::IVideoDriver *drv;
+    
+    //FUNCTIONS
+public:
+    //constructor 
+    Ship(float movementSpeed, int firingSpeed, int movementDirecion, irr::ITimer *timerReference, const irr::io::path &pathOfMesh, const irr::io::path &pathOfTexture, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference, bool spawnOnConstruct = true);
+    //destructor
+    ~Ship();
+    
+    //override
+    virtual void tick(irr::f32 deltaTime);
+    
+protected:
+    //makes the ship shoot
+    void shoot();    
+};
 
 #endif	/* SHIP_H */
 
