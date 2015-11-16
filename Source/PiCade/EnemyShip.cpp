f317aaab5d14d75899d99a6b8b05ae4e285847d2
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-16 16:36:35 +0000

    enemy ships basic shooting implemented - current bug with the range check however

--- Source/PiCade/EnemyShip.cpp
+++ Source/PiCade/EnemyShip.cpp
@@ -1,8 +1,36 @@
 # include "EnemyShip.h"
 
-EnemyShip::EnemyShip(irr::core::vector3df spawnPosition, float movementSpeed, int firingSpeed, irr::ITimer* timerReference, const irr::io::path& pathOfMesh, const irr::io::path& pathOfTexture, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
+EnemyShip::EnemyShip(PlayerShip *player, irr::core::vector3df spawnPosition, float movementSpeed, int firingSpeed, irr::ITimer* timerReference, const irr::io::path& pathOfMesh, const irr::io::path& pathOfTexture, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
         : Ship(movementSpeed, firingSpeed, -1, timerReference, pathOfMesh, pathOfTexture, sceneManagerReference, driverReference){
     
-    //set the position
+    //set the spawn position
     objectPosition = spawnPosition;
+    
+    //set the player
+    playerTarget = player;
+    
+    //init variables
+    combatDistance = 100.0f;
+}
+
+EnemyShip::~EnemyShip(){
+    //clear the pointer, data gets deleted elsewhere
+    playerTarget = 0;
+    
+    //delete pointers
+    delete playerTarget;
+}
+
+void EnemyShip::tick(irr::f32 deltaTime){
+    //call the base class function
+    Ship::tick(deltaTime);
+    
+    //check the distance of the player
+    if(computeDistance(getPosition(), playerTarget->getPosition()) <= combatDistance){
+        shoot();
+    }
+}
+
+float EnemyShip::computeDistance(irr::core::vector3df positionA, irr::core::vector3df positionB){
+    return sqrt((pow(positionB.X, 2) - pow(positionA.X, 2)) + (pow(positionB.Y, 2) - pow(positionA.Y, 2)) + (pow(positionB.Z, 2) - pow(positionA.Z, 2)));
 }
--- Source/PiCade/EnemyShip.h
+++ Source/PiCade/EnemyShip.h
@@ -7,14 +7,32 @@
 #define	ENEMYSHIP_H
 
 #include "Ship.h"
+#include "PlayerShip.h"
+
+#include <cmath>
 
 class EnemyShip : public Ship{
     //VARIABLES
+private:
+    //the player, the enemy's target
+    PlayerShip *playerTarget;
+    
+    //how far the player needs to be for the enemy to engage
+    float combatDistance;
     
     //FUNCTIONS
 public:
     //constructor
-    EnemyShip(irr::core::vector3df spawnPos, float movementSpeed, int firingSpeed, irr::ITimer *timerReference, const irr::io::path &pathOfMesh, const irr::io::path &pathOfTexture, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference);
+    EnemyShip(PlayerShip *player, irr::core::vector3df spawnPos, float movementSpeed, int firingSpeed, irr::ITimer *timerReference, const irr::io::path &pathOfMesh, const irr::io::path &pathOfTexture, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference);
+    //destructor
+    ~EnemyShip();
+    
+    //override
+    virtual void tick(irr::f32 deltaTime);
+    
+private:
+    //computes the distance between two objects
+    float computeDistance(irr::core::vector3df positionA, irr::core::vector3df positionB);
 };
 
 #endif	/* ENEMYSHIP_H */
