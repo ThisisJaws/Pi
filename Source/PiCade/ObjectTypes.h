9ef7f02bdaa444207e5f3b4f54d5d0bc42568ecd
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-21 11:09:56 +0000

    fixed define header

--- Source/PiCade/ObjectTypes.h
+++ Source/PiCade/ObjectTypes.h
@@ -8,12 +8,12 @@
 #ifndef OBJECTTYPES_H
 #define	OBJECTTYPES_H
 
-#define TYPE_UNDEFINED_TYPE = 0;
-#define TYPE_PLAYER = 1;
-#define TYPE_ENEMY = 2;
-#define TYPE_COLLECTABLE = 3;
-#define TYPE_STATIC = 4;
-#define TYPE_MODE_CHANGER = 5;
+#define TYPE_UNDEFINED_TYPE  0;
+#define TYPE_PLAYER          1;
+#define TYPE_ENEMY           2;
+#define TYPE_COLLECTABLE     3;
+#define TYPE_STATIC          4;
+#define TYPE_MODE_CHANGER    5;
 
 #endif	/* OBJECTTYPES_H */
 
