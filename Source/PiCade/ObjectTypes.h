c520614d85f331b44b174cff6013d46768908e38
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-21 11:18:53 +0000

    changed type definitions again

--- Source/PiCade/ObjectTypes.h
+++ Source/PiCade/ObjectTypes.h
@@ -8,12 +8,13 @@
 #ifndef OBJECTTYPES_H
 #define	OBJECTTYPES_H
 
-#define TYPE_UNDEFINED_TYPE  0;
-#define TYPE_PLAYER          1;
-#define TYPE_ENEMY           2;
-#define TYPE_COLLECTABLE     3;
-#define TYPE_STATIC          4;
-#define TYPE_MODE_CHANGER    5;
+#define TYPE_UNDEFINED_TYPE 0
+#define TYPE_PLAYER         1
+#define TYPE_ENEMY          2
+#define TYPE_BULLET         3
+#define TYPE_COLLECTABLE    4
+#define TYPE_STATIC_OBJECT  5
+#define TYPE_MODE_CHANGER   6
 
 #endif	/* OBJECTTYPES_H */
 
