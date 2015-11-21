1c18617b2dda114bcc38d19ba1d2b18ea021158d
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-21 11:03:38 +0000

    moved the object type definitions to a seperate header file

--- Source/PiCade/Object.h
+++ Source/PiCade/Object.h
@@ -8,20 +8,8 @@
 #ifndef OBJECT_H
 #define	OBJECT_H
 
-/* 
- * these defines are used in collision
- * when colliding with an object you can use
- * these defines to check what the derived class
- * is, think of it as an ID
- */
-#define TYPE_UNDEFINED_TYPE = 0;
-#define TYPE_PLAYER = 1;
-#define TYPE_ENEMY = 2;
-#define TYPE_COLLECTABLE = 3;
-#define TYPE_STATIC = 4;
-#define TYPE_MODE_CHANGER = 5;
-
 #include "irrlicht.h"
+#include "ObjectTypes.h"
 #include <list>
 
 class Object{
---
+++ Source/PiCade/ObjectTypes.h
@@ -0,0 +1,19 @@
+/* 
+ * these defines are used in collision
+ * when colliding with an object you can use
+ * these defines to check what the derived class
+ * is, think of it as an ID
+ */
+
+#ifndef OBJECTTYPES_H
+#define	OBJECTTYPES_H
+
+#define TYPE_UNDEFINED_TYPE = 0;
+#define TYPE_PLAYER = 1;
+#define TYPE_ENEMY = 2;
+#define TYPE_COLLECTABLE = 3;
+#define TYPE_STATIC = 4;
+#define TYPE_MODE_CHANGER = 5;
+
+#endif	/* OBJECTTYPES_H */
+
