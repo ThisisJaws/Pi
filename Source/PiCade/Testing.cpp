621d6ecacefe79a1950456ca0acf7b6f4c42a4a6
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-23 22:30:59 +0000

    testing Textures

--- Source/PiCade/Testing.cpp
+++ Source/PiCade/Testing.cpp
@@ -11,21 +11,22 @@ int Testing::runTest(){
     IVideoDriver *driver = device->getVideoDriver();
     ISceneManager *smgr = device->getSceneManager();
     
-    IAnimatedMesh *testMesh = smgr->getMesh("Assets/ship 1 obj.obj");
+    IAnimatedMesh *testMesh = smgr->getMesh("Assets/Ships/PlayerShip.obj");
     if(!testMesh){
         device->drop();
         return 1;
     }
         
     int arraySize = 1;      //CHANGE THIS TO ADD SHIPS TO THE SCENE
-    int xOffSet = 80;       //CHANGE THIS TO THE LENGTH OF THE SHIPS
-    int zOffSet = 80;       //CHNAGE THIS TO THE WIDTH OF THE SHIPS
+    int xOffSet = 0;       //CHANGE THIS TO THE LENGTH OF THE SHIPS
+    int zOffSet = 0;       //CHNAGE THIS TO THE WIDTH OF THE SHIPS
     
     int a = 0;
     vector3df nodePosition = vector3df(0, 0, zOffSet);
     IAnimatedMeshSceneNode *testMeshNodes[arraySize];
     for(int i = 0; i < arraySize; i++){
         testMeshNodes[i] = smgr->addAnimatedMeshSceneNode(testMesh);
+        testMeshNodes[i]->setMaterialTexture(0, driver->getTexture("Assets/Ships/PlayerShipTexture.bmp"));
         testMeshNodes[i]->setMaterialFlag(EMF_LIGHTING, false);
         if(a > 0){
             nodePosition.X += xOffSet;
