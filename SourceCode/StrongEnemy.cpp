429ba8f3c73a67fcf4e7b3a2bc1c2fa0c36720a2
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-26 16:22:49 +0000

    added in basic characteristics on the enemy ships

--- SourceCode/StrongEnemy.cpp
+++ SourceCode/StrongEnemy.cpp
@@ -1,6 +1,16 @@
 #include "StrongEnemy.h"
 
 StrongEnemy::StrongEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 30.0f, 250, timerReference, "Assets/Ships/EnemyShips/EnemyShip2.obj", "Assets/PlaceHolders/ship 1 obj.mtl", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 30.0f, 500, timerReference, "Assets/Ships/EnemyShips/EnemyShip1.obj", "Assets/PlaceHolders/ship 1 obj.mtl", sceneManagerReference, driverReference){
     
+    hitsTaken = 0;
+    
+}
+
+void StrongEnemy::markForDelete(){
+    hitsTaken++;
+    
+    if(hitsTaken >= 2){
+        EnemyShip::markForDelete();
+    }
 }
--- SourceCode/StrongEnemy.h
+++ SourceCode/StrongEnemy.h
@@ -8,11 +8,18 @@
 #include "EnemyShip.h"
 
 class StrongEnemy : public EnemyShip{
+    //VARIABLES
+public:
+    //strong enemy takes two hits to kill
+    int hitsTaken;
+    
     //FUNCTIONS
 public:
     //constructor
     StrongEnemy(PlayerShip *player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference);
     
+    //override
+    void markForDelete();
 };
 
 #endif	/* STRONGENEMY_H */
