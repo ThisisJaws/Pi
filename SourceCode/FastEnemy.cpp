682932c743208664f018f37fa3754642a1d57ce0
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-26 16:28:57 +0000

    added basic fast enemy characteristics

--- SourceCode/FastEnemy.cpp
+++ SourceCode/FastEnemy.cpp
@@ -1,6 +1,6 @@
 #include "FastEnemy.h"
 
 FastEnemy::FastEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 30.0f, 250, timerReference, "Assets/Ships/EnemyShips/EnemyShip3.obj", "Assets/PlaceHolders/ship 1 obj.mtl", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 60.0f, 125, timerReference, "Assets/Ships/EnemyShips/EnemyShip3.obj", "Assets/PlaceHolders/ship 1 obj.mtl", sceneManagerReference, driverReference){
     
 }
