37b0eada0e1680d15e3c542379d2cc000f2d24b3
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-27 17:22:29 +0000

    put the right file path

--- SourceCode/FastEnemy.cpp
+++ SourceCode/FastEnemy.cpp
@@ -1,6 +1,6 @@
 #include "FastEnemy.h"
 
 FastEnemy::FastEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 60.0f, 125, timerReference, "Assets/Ships/EnemyShips/FastShip.obj", "Assets/Ships/EnemyShips/FastShip.bmp", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 60.0f, 125, timerReference, "Assets/Ships/EnemyShips/FastShip.obj", "Assets/Ships/EnemyShips/FastShipTexture.bmp", sceneManagerReference, driverReference){
     
 }
