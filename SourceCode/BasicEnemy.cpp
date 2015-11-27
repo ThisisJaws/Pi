9e78c88084e1ff45e8200a2ec142be6431a0ba38
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-27 16:47:39 +0000

    added in proper enemy models

--- SourceCode/BasicEnemy.cpp
+++ SourceCode/BasicEnemy.cpp
@@ -1,6 +1,6 @@
 #include "BasicEnemy.h"
 
 BasicEnemy::BasicEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 30.0f, 250, timerReference, "Assets/Ships/EnemyShips/EnemyShip2.obj", "Assets/PlaceHolders/ship 1 obj.mtl", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 30.0f, 250, timerReference, "Assets/Ships/EnemyShips/BasicShip.obj", "Assets/Ships/EnemyShips/BasicShipTexture.bmp", sceneManagerReference, driverReference){
     
 }
--- SourceCode/StrongEnemy.cpp
+++ SourceCode/StrongEnemy.cpp
@@ -1,7 +1,7 @@
 #include "StrongEnemy.h"
 
 StrongEnemy::StrongEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 30.0f, 500, timerReference, "Assets/Ships/EnemyShips/EnemyShip1.obj", "Assets/PlaceHolders/ship 1 obj.mtl", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 30.0f, 500, timerReference, "Assets/Ships/EnemyShips/StrongShip.obj", "Assets/Ships/EnemyShips/StrongShipTexture.bmp", sceneManagerReference, driverReference){
     
     hitsTaken = 0;
     
