fe2a4fd924b4acd117bfbdfcffa5853d773944b9
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-11-27 17:07:47 +0000

    fixed files paths

--- SourceCode/FastEnemy.cpp
+++ SourceCode/FastEnemy.cpp
@@ -1,6 +1,6 @@
 #include "FastEnemy.h"
 
 FastEnemy::FastEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 60.0f, 125, timerReference, "Assets/Ships/EnemyShips/FastShip.obj", "Assets/Ship/EnemyShips/FastShips/bmp", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 60.0f, 125, timerReference, "Assets/Ships/EnemyShips/FastShip.obj", "Assets/Ship/EnemyShips/FastShip.bmp", sceneManagerReference, driverReference){
     
 }
--- SourceCode/PlayerShip.cpp
+++ SourceCode/PlayerShip.cpp
@@ -1,7 +1,7 @@
 #include "PlayerShip.h"
 
 PlayerShip::PlayerShip(EventReceiver *eReceiver, irr::ITimer *timerReference, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference) 
-    : Ship(150.0f, 250, 1, timerReference, "Assets/Ships/PlayerShip.obj", "Assets/Ships/PlayerShip.bmp", sceneManagerReference, driverReference){
+    : Ship(150.0f, 250, 1, timerReference, "Assets/Ships/PlayerShip.obj", "Assets/Ships/PlayerShipTexture.bmp", sceneManagerReference, driverReference){
     
     //temp - rotate
     getSceneNode()->setRotation(irr::core::vector3df(0, 180, 0));
