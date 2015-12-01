177f9de11c90df0f6879200f27660e6c9b337e49
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-01 15:59:13 +0000

    added in collectable base class (empty)

---
+++ SourceCode/Collectable.h
@@ -0,0 +1,17 @@
+/* 
+ * The base class for all the collectables
+ * all the individual properties for the collectables
+ * will be in classes that inherit from this 
+ */
+
+#ifndef COLLLECTABLE_H
+#define	COLLLECTABLE_H
+
+#include "Object.h"
+
+class Collectable : public Object{
+    
+};
+
+#endif	/* COLLLECTABLE_H */
+
