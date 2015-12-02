71275e0ef344313a115c2e1e7a5689ee5945812d
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-02 21:24:42 +0000

    added texture and mesh

--- SourceCode/Gem.cpp
+++ SourceCode/Gem.cpp
@@ -1,7 +1,7 @@
 #include "Gem.h"
 
 Gem::Gem(irr::core::vector3df spawnPosition, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference)
-        : Collectable(spawnPosition, "Mesh", "Texture", sceneManagerReference, driverReference){
+        : Collectable(spawnPosition, "Assets/Collectables/SpaceGem.obj", "Assets/Collectables/SpaceGemOrange.bmp", sceneManagerReference, driverReference){
     
     scoreIncrease = 100;
 }
