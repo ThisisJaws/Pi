f157c3e1518ab6b7e3f4dc4bff4d4a39d5348921
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2015-12-02 15:55:06 +0000

    Commenting out lines so it will compile for windows

--- SourceCode/Testing.cpp
+++ SourceCode/Testing.cpp
@@ -17,17 +17,17 @@ int Testing::runTest(){
         return 1;
     }
         
-    int arraySize = 1;      //CHANGE THIS TO ADD SHIPS TO THE SCENE
+    int const arraySize = 1;      //CHANGE THIS TO ADD SHIPS TO THE SCENE
     int xOffSet = 0;       //CHANGE THIS TO THE LENGTH OF THE SHIPS
     int zOffSet = 0;       //CHNAGE THIS TO THE WIDTH OF THE SHIPS
     
     int a = 0;
     vector3df nodePosition = vector3df(0, 0, zOffSet);
-    IAnimatedMeshSceneNode *testMeshNodes[arraySize];
+    //IAnimatedMeshSceneNode *testMeshNodes[arraySize];
     for(int i = 0; i < arraySize; i++){
-        testMeshNodes[i] = smgr->addAnimatedMeshSceneNode(testMesh);
-        testMeshNodes[i]->setMaterialTexture(0, driver->getTexture("Assets/Ships/PlayerShipTexture.bmp"));
-        testMeshNodes[i]->setMaterialFlag(EMF_LIGHTING, false);
+        //testMeshNodes[i] = smgr->addAnimatedMeshSceneNode(testMesh);
+        //testMeshNodes[i]->setMaterialTexture(0, driver->getTexture("Assets/Ships/PlayerShipTexture.bmp"));
+        //testMeshNodes[i]->setMaterialFlag(EMF_LIGHTING, false);
         if(a > 0){
             nodePosition.X += xOffSet;
             if(a > 7){
@@ -36,7 +36,7 @@ int Testing::runTest(){
                 a = 0;
             }
         }
-        testMeshNodes[i]->setPosition(nodePosition);
+        //testMeshNodes[i]->setPosition(nodePosition);
         a++;
     }
     
