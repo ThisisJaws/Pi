6bebe8ddcffaac113c45eabae8a839f0ece05ccc
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-05 18:32:36 +0000

    adding the new ogl-es files to repo

---
+++ Irrlicht_1.9.0_OGL-ES/source/Irrlicht/CMeshTextureLoader.d
@@ -0,0 +1,24 @@
+CMeshTextureLoader.o: CMeshTextureLoader.cpp CMeshTextureLoader.h \
+ ../../include/IMeshTextureLoader.h ../../include/path.h \
+ ../../include/irrString.h ../../include/irrTypes.h \
+ ../../include/IrrCompileConfig.h ../../include/irrAllocator.h \
+ ../../include/irrMath.h ../../include/IReferenceCounted.h \
+ ../../include/IFileSystem.h ../../include/IXMLReader.h \
+ ../../include/irrXML.h ../../include/IFileArchive.h \
+ ../../include/IReadFile.h ../../include/coreutil.h \
+ ../../include/IFileList.h ../../include/IVideoDriver.h \
+ ../../include/rect.h ../../include/dimension2d.h \
+ ../../include/position2d.h ../../include/vector2d.h \
+ ../../include/SColor.h ../../include/ITexture.h ../../include/IImage.h \
+ ../../include/EDriverTypes.h ../../include/matrix4.h \
+ ../../include/vector3d.h ../../include/plane3d.h \
+ ../../include/aabbox3d.h ../../include/line3d.h ../../include/irrArray.h \
+ ../../include/heapsort.h ../../include/SMaterial.h \
+ ../../include/EMaterialTypes.h ../../include/EMaterialFlags.h \
+ ../../include/SMaterialLayer.h ../../include/IMeshBuffer.h \
+ ../../include/S3DVertex.h ../../include/SVertexIndex.h \
+ ../../include/EHardwareBufferFlags.h ../../include/EPrimitiveTypes.h \
+ ../../include/triangle3d.h ../../include/EDriverFeatures.h \
+ ../../include/SExposedVideoData.h os.h ../../include/IrrCompileConfig.h \
+ ../../include/irrTypes.h ../../include/irrString.h ../../include/path.h \
+ ../../include/ILogger.h ../../include/ITimer.h
---
+++ Irrlicht_1.9.0_OGL-ES/source/Irrlicht/COGLES2ExtensionHandler.d
@@ -0,0 +1,45 @@
+COGLES2ExtensionHandler.o: COGLES2ExtensionHandler.cpp \
+ ../../include/IrrCompileConfig.h COGLES2ExtensionHandler.h CEGLManager.h \
+ ../../include/SIrrCreationParameters.h ../../include/EDriverTypes.h \
+ ../../include/irrTypes.h ../../include/IrrCompileConfig.h \
+ ../../include/EDeviceTypes.h ../../include/dimension2d.h \
+ ../../include/irrMath.h ../../include/ILogger.h \
+ ../../include/IReferenceCounted.h ../../include/position2d.h \
+ ../../include/vector2d.h ../../include/path.h ../../include/irrString.h \
+ ../../include/irrAllocator.h ../../include/SExposedVideoData.h \
+ ../../include/IContextManager.h ../../include/SExposedVideoData.h \
+ ../../include/SIrrCreationParameters.h /opt/vc/include/GLES2/gl2.h \
+ /opt/vc/include/GLES2/gl2platform.h \
+ /opt/vc/include/GLES2/../KHR/khrplatform.h gles2-ext.h os.h \
+ ../../include/irrTypes.h ../../include/irrString.h ../../include/path.h \
+ ../../include/ILogger.h ../../include/ITimer.h \
+ ../../include/EDriverFeatures.h COGLES2Driver.h CNullDriver.h \
+ ../../include/IVideoDriver.h ../../include/rect.h ../../include/SColor.h \
+ ../../include/ITexture.h ../../include/IImage.h ../../include/matrix4.h \
+ ../../include/vector3d.h ../../include/plane3d.h \
+ ../../include/aabbox3d.h ../../include/line3d.h ../../include/irrArray.h \
+ ../../include/heapsort.h ../../include/SMaterial.h \
+ ../../include/EMaterialTypes.h ../../include/EMaterialFlags.h \
+ ../../include/SMaterialLayer.h ../../include/IMeshBuffer.h \
+ ../../include/S3DVertex.h ../../include/SVertexIndex.h \
+ ../../include/EHardwareBufferFlags.h ../../include/EPrimitiveTypes.h \
+ ../../include/triangle3d.h ../../include/EDriverFeatures.h \
+ ../../include/IFileSystem.h ../../include/IXMLReader.h \
+ ../../include/irrXML.h ../../include/IFileArchive.h \
+ ../../include/IReadFile.h ../../include/coreutil.h \
+ ../../include/IFileList.h IImagePresenter.h ../../include/IImage.h \
+ ../../include/IGPUProgrammingServices.h ../../include/EShaderTypes.h \
+ ../../include/irrArray.h ../../include/irrMap.h \
+ ../../include/IAttributes.h ../../include/line2d.h \
+ ../../include/quaternion.h ../../include/EAttributes.h \
+ ../../include/IMesh.h ../../include/IMeshBuffer.h \
+ ../../include/IMeshSceneNode.h ../../include/ISceneNode.h \
+ ../../include/IAttributeExchangingObject.h \
+ ../../include/ESceneNodeTypes.h ../../include/ECullingTypes.h \
+ ../../include/EDebugSceneTypes.h ../../include/ISceneNodeAnimator.h \
+ ../../include/ESceneNodeAnimatorTypes.h ../../include/IAttributes.h \
+ ../../include/IEventReceiver.h ../../include/Keycodes.h \
+ ../../include/ITriangleSelector.h ../../include/irrList.h CFPSCounter.h \
+ ../../include/S3DVertex.h ../../include/SVertexIndex.h \
+ ../../include/SLight.h ../../include/IMaterialRendererServices.h \
+ ../../include/fast_atof.h
