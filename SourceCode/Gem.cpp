ece8c9146b6e3c90ede56ea09bc2a9adafe2367c
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2015-12-06 18:24:56 +0000

    Gives more score

--- SourceCode/Gem.cpp
+++ SourceCode/Gem.cpp
@@ -3,7 +3,7 @@
 Gem::Gem(irr::core::vector3df spawnPosition, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference)
         : Collectable(spawnPosition, "Assets/Collectables/SpaceGem.obj", "Assets/Collectables/SpaceGem_Orange.bmp", sceneManagerReference, driverReference){
 
-    scoreIncrease = 100;
+    scoreIncrease = 1000;
 }
 
 void Gem::activate(PlayerShip* player){
