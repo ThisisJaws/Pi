749e216fc45d35b9dc3d86a7a4b490e97d1a2860
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-06 10:38:31 +0000

    Added texture to gems

--- SourceCode/Gem.cpp
+++ SourceCode/Gem.cpp
@@ -1,8 +1,8 @@
 #include "Gem.h"
 
 Gem::Gem(irr::core::vector3df spawnPosition, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference)
-        : Collectable(spawnPosition, "Assets/Collectables/SpaceGem.obj", "Assets/Collectables/SpaceGemOrange.bmp", sceneManagerReference, driverReference){
-    
+        : Collectable(spawnPosition, "Assets/Collectables/SpaceGem.obj", "Assets/Collectables/SpaceGem_Orange.bmp", sceneManagerReference, driverReference){
+
     scoreIncrease = 100;
 }
 
