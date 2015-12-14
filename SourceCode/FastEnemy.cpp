ffdb99ca283619df772f245f5f837b6dfc2ae554
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-14 15:27:34 +0000

    First combat manouver implemented

--- SourceCode/FastEnemy.cpp
+++ SourceCode/FastEnemy.cpp
@@ -1,8 +1,49 @@
 #include "FastEnemy.h"
 
 FastEnemy::FastEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 125, timerReference, "Assets/Ships/EnemyShips/FastShip.obj", "Assets/Ships/EnemyShips/FastShipTexture.jpg", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 500, timerReference, "Assets/Ships/EnemyShips/FastShip.obj", "Assets/Ships/EnemyShips/FastShipTexture.jpg", sceneManagerReference, driverReference){
 
+	//Start at the top of the screen
+	changePosition(irr::core::vector3df(getPosition().X, 45, getPosition().Z));
+
+	currentStage = stageStart;
+	timeElapsed = 0;
 }
 
-void FastEnemy::combatManouver(irr::f32 deltaTime){}
+void FastEnemy::combatManouver(irr::f32 deltaTime){
+	//Shoot constantly
+	shoot(irr::core::vector3df(0, 0, moveDir));
+
+	//Decide which stage to perform
+	switch(currentStage){
+		case stageStart:
+			//Move slowly forward
+			moveSpeed -= 20;
+			currentStage = stageA;
+			break;
+
+		case stageA:
+			combatStageA(deltaTime);
+			break;
+
+		case stageB:
+			combatStageB(deltaTime);
+			break;
+	}
+}
+
+void FastEnemy::combatStageA(irr::f32 deltaTime){
+	if(getPosition().Y > -45){
+		moveDown(turnSpeed, deltaTime);
+	} else{
+		currentStage = stageB;
+	}
+}
+
+void FastEnemy::combatStageB(irr::f32 deltaTime){
+	if(getPosition().Y < 45){
+		moveUp(turnSpeed, deltaTime);
+	} else{
+		currentStage = stageA;
+	}
+}
--- SourceCode/FastEnemy.h
+++ SourceCode/FastEnemy.h
@@ -8,13 +8,30 @@
 #include "EnemyShip.h"
 
 class FastEnemy : public EnemyShip{
-    //FUNCTIONS
+    //VARIABLES
+private:
+	//What stage the manouver is in
+	enum combatStage{
+		stageStart,
+		stageA,
+		stageB,
+	}currentStage;
+
+	//Time (deltatime) elapsed
+	float timeElapsed;
+	
+	//FUNCTIONS
 public:
     //constructor
     FastEnemy(PlayerShip *player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference);
     
 	//What the enemy will do when in range of the player
 	virtual void combatManouver(irr::f32 deltaTime) override;
+
+private:
+	//Combat manouver stages
+	void combatStageA(irr::f32 deltaTime);
+	void combatStageB(irr::f32 deltaTime);
 };
 
 #endif	/* FASTENEMY_H */
