5d721f3a2325aed5d405a77cc0491ea87a09872d
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-14 12:31:23 +0000

    Removed old code

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -111,19 +111,6 @@ int main(int argc, char** argv) {
     //Drop the device to end the program
     device->drop();
 
-    //create a game object - everything gets initialised in the constructor
-    //Game currentGame = Game();
-
-	//bool exit = false;
-
-	//do{
-	//	exit = currentGame.play();
-	//} while(!exit);
-
-    //once the loop breaks go straight to clean up (for now)
-    //currentGame.cleanUp();
-
-    //once the 'play' loop has ended then exit program
     return 0;
 }
 
