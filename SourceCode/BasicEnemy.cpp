72f98dfd8a9e970b79ffd9256d9f2164340e4d28
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-14 14:46:01 +0000

    Reduced rate of fire

--- SourceCode/BasicEnemy.cpp
+++ SourceCode/BasicEnemy.cpp
@@ -1,7 +1,7 @@
 #include "BasicEnemy.h"
 
 BasicEnemy::BasicEnemy(PlayerShip* player, irr::core::vector3df spawnPosition, irr::ITimer* timerReference, irr::scene::ISceneManager* sceneManagerReference, irr::video::IVideoDriver* driverReference)
-            : EnemyShip(player, spawnPosition, 320, timerReference, "Assets/Ships/EnemyShips/BasicShip.obj", "Assets/Ships/EnemyShips/BasicShipTexture.jpg", sceneManagerReference, driverReference){
+            : EnemyShip(player, spawnPosition, 750, timerReference, "Assets/Ships/EnemyShips/BasicShip.obj", "Assets/Ships/EnemyShips/BasicShipTexture.jpg", sceneManagerReference, driverReference){
 
 	currentStage = stageA;
 	timeElapsed = 0;
