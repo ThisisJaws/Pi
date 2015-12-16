fb8ebaf79665088611f8728151eeff854978d497
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-16 15:47:42 +0000

    Forgot to commit

--- SourceCode/LavaWorld.h
+++ SourceCode/LavaWorld.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef LAVAWORLD_H
+#define	LAVAWORLD_H
+
+#include "World.h"
+
+class LavaWorld : public World{
+	//VARIABLES
+
+	//FUNCTIONS
+public:
+	//constructor
+	LavaWorld(PlayerShip *player);
+
+	virtual void loadPhase1(irr::IrrlichtDevice *device, std::list<Object*> *objectUpdateList) override;
+	virtual void loadPhase2(irr::IrrlichtDevice *device, std::list<Object*> *objectUpdateList) override;
+};
+
+
+#endif // ! LAVAWORLD_H
