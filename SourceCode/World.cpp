84bbaed702f594c52664a44f819533edc88e638f
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-16 15:02:21 +0000

    Added win condition for phase 2

--- SourceCode/World.cpp
+++ SourceCode/World.cpp
@@ -20,7 +20,15 @@ bool World::isPhase1Complete(){
 bool World::isPhase2Complete(){
 	//Check if the player has completed phase 2
 	if(phase2Loaded){
+		for(std::list<Object*>::iterator shipIterator = shipReferences.begin(); shipIterator != shipReferences.end(); ++shipIterator){
+			if(*shipIterator != NULL){
+				//If any ship reference is still there the player hasnt completed the level
+				return false;
+			}
+		}
 
+		//if the loop completes then the level is complete
+		return true;
 	} else{
 		return false;
 	}
