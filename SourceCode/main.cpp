a4a51b93526cbd2e0cab2bc6ab466905a68a0210
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-31 13:07:27 +0000

    Version change for new ice world

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -18,8 +18,8 @@
 
 //Defines for version number
 #define CURRENT_VERSION_MAJOR	 0
-#define CURRENT_VERSION_MINOR	 1
-#define CURRENT_VERSION_REVISION 2
+#define CURRENT_VERSION_MINOR	 2
+#define CURRENT_VERSION_REVISION 0
 
 /*
  * program entry point
