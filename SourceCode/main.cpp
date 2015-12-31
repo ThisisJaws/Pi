1c15941457ea6d664d2cb9132ba896bb51498b3d
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-31 12:03:11 +0000

    Increase revision

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -19,7 +19,7 @@
 //Defines for version number
 #define CURRENT_VERSION_MAJOR	 0
 #define CURRENT_VERSION_MINOR	 1
-#define CURRENT_VERSION_REVISION 0
+#define CURRENT_VERSION_REVISION 1
 
 /*
  * program entry point
