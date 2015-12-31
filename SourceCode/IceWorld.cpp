1a54cf1e8a052c84e0284b9f2790e6ac9565cd52
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2015-12-31 13:39:51 +0000

    Tweak to get the phase loading properly

--- SourceCode/IceWorld.cpp
+++ SourceCode/IceWorld.cpp
@@ -14,7 +14,7 @@ void IceWorld::loadPhase1(irr::IrrlichtDevice * device){
 	terrain = loadTerrain(device,
 						  "Assets/Environment/Levels/IceWorldHeightMap512x512.jpg",
 						  driver->getTexture("Assets/Environment/Levels/IceWorldTexture.jpg"),
-						  irr::core::vector3df(10, 2, 10));
+						  irr::core::vector3df(1, 1, 1));
 
 	//Reset the player position
 	player->changePosition(irr::core::vector3df(0, 0, 0));
@@ -26,6 +26,9 @@ void IceWorld::loadPhase2(irr::IrrlichtDevice * device){
 	if(terrain != NULL){
 		terrain->remove();
 	}
+
+	//Make sure the previous phase is no longer considered loaded
+	phase1Loaded = false;
 
 	//Get the references
 	irr::scene::ISceneManager *smgr = device->getSceneManager();
