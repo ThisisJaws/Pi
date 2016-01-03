eab299a1800fff6a35670dcfef1ca8d973a5855e
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2016-01-03 15:18:29 +0000

    Constructor now loads in segments

--- SourceCode/IceWorld.cpp
+++ SourceCode/IceWorld.cpp
@@ -5,7 +5,18 @@
 IceWorld::IceWorld(PlayerShip *player) 
 	: World(player){
 
-	phase1StartPosition = irr::core::vector3df(5100, 170, -500);
+	//Set the player start pos
+	phase1StartPosition = irr::core::vector3df(256, 60, -500);
+
+	//Load in all the file paths
+	//heightMapLocations[0] = "Assets/Environment/Levels/IceWorld/HeightMap-Piece1-512x512.jpg";
+	//heightMapLocations[1] = "Assets/Environment/Levels/IceWorld/HeightMap-Piece2-512x512.jpg";
+	//heightMapLocations[2] = "Assets/Environment/Levels/IceWorld/Land/HeightMap-Piece3-512x512.jpg";
+	//heightMapLocations[3] = "Assets/Environment/Levels/IceWorld/HeightMap-Piece4-512x512.jpg";
+	//heightMapLocations[4] = "Assets/Environment/Levels/IceWorld/HeightMap-Piece5-512x512.jpg";
+
+	//Load in the texture's file path
+	terrainTexturePath = "Assets/Environment/Levels/IceWorld/IceWorldTexture.jpg";
 }
 
 void IceWorld::loadPhase1Obsticles(const irr::core::vector3df & playerStartPos, irr::scene::ISceneManager * sceneManager, irr::video::IVideoDriver * videoDriver){
--- SourceCode/JungleWorld.cpp
+++ SourceCode/JungleWorld.cpp
@@ -5,7 +5,18 @@
 JungleWorld::JungleWorld(PlayerShip *player)
 	: World(player){
 
-	phase1StartPosition = irr::core::vector3df(4920, 190, -500);
+	//Set the player start pos
+	phase1StartPosition = irr::core::vector3df(256, 60, -500);
+
+	//Load in all the file paths
+	heightMapLocations[0] = "Assets/Environment/Levels/JungleWorld/HeightMap-Piece1-512x512.jpg";
+	heightMapLocations[1] = "Assets/Environment/Levels/JungleWorld/HeightMap-Piece2-512x512.jpg";
+	heightMapLocations[2] = "Assets/Environment/Levels/JungleWorld/HeightMap-Piece3-512x512.jpg";
+	heightMapLocations[3] = "Assets/Environment/Levels/JungleWorld/HeightMap-Piece4-512x512.jpg";
+	heightMapLocations[4] = "Assets/Environment/Levels/JungleWorld/HeightMap-Piece5-512x512.jpg";
+
+	//Load in the texture's file path
+	terrainTexturePath = "Assets/Environment/Levels/JungleWorld/JungleWorldTexture.jpg";
 }
 
 void JungleWorld::loadPhase1Obsticles(const irr::core::vector3df & playerStartPos, irr::scene::ISceneManager * sceneManager, irr::video::IVideoDriver * videoDriver){
