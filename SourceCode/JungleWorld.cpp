ffa8f70c694547ca8b3f206d28d3e286a2e32b18
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-06 22:05:04 +0000

    Adding some sort of island for now

--- SourceCode/JungleWorld.cpp
+++ SourceCode/JungleWorld.cpp
@@ -17,7 +17,28 @@ JungleWorld::JungleWorld(PlayerShip *player)
 }
 
 void JungleWorld::loadPhase1Obsticles(const irr::core::vector3df & playerStartPos, irr::scene::ISceneManager * sceneManager, irr::video::IVideoDriver * videoDriver){
+	//The pointer that will init all the islands into memory then be passed into the list
+	StaticObject *floatingIsland;
 
+	//Where the island will spawn
+	irr::core::vector3df spawnPos = playerStartPos;
+
+	//The path of the mesh and textures
+	irr::io::path meshPath = "Assets/Environment/FloatingIslands/FloatingIsland1.obj";
+	irr::io::path texturePath = "Assets/Environment/FloatingIslands/FloatIslandsTexture.jpg";
+
+	//1
+	//Get the spawn based off the player start position
+	spawnPos.Z += 400;
+	spawnPos.X += 0;
+	spawnPos.Y += 30;
+	//Create the new object
+	floatingIsland = new StaticObject(spawnPos, meshPath, texturePath, sceneManager, videoDriver, false);
+	//Put it on the update list
+	Game::addObjectToUpdate(floatingIsland);
+	//Reset the X and Y back to zero
+	spawnPos.X = playerStartPos.X;
+	spawnPos.Y = playerStartPos.Y;
 }
 
 void JungleWorld::loadPhase1Gems(const irr::core::vector3df & playerStartPos, irr::scene::ISceneManager * sceneManager, irr::video::IVideoDriver * videoDriver){
