82ff343527139ce433518780560298049f146b55
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-06 17:12:06 +0000

    Terrain generation tweaked

--- SourceCode/World.cpp
+++ SourceCode/World.cpp
@@ -29,7 +29,7 @@ void World::loadPhase1(irr::IrrlichtDevice * device){
 		//Random number for which tile to load
 		int tile = rand() % HEIGHT_MAP_COUNT;
 		//Create a terrain scene node using the tile selection
-		terrainNodes[i] = loadTerrain(device, heightMapLocations[tile], driver->getTexture(terrainTexturePath), terrainPos, irr::core::vector3df(1, 1, 1));
+		terrainNodes[i] = loadTerrain(device, heightMapLocations[tile], driver->getTexture(terrainTexturePath), terrainPos, irr::core::vector3df(1, 1, 2));
 		//Get an array to hold all of the edges
 		irr::core::vector3d<irr::f32> edges[8];
 		//Get the bounding box of the mesh
@@ -37,7 +37,7 @@ void World::loadPhase1(irr::IrrlichtDevice * device){
 		//Get the edges of the box
 		boundingBox.getEdges(edges);
 		//Increase the starting pos by the length of the box
-		terrainPos.Z += (edges[2].Z - edges[0].Z) / terrainNodes[i]->getScale().Z;
+		terrainPos.Z += (edges[2].Z - edges[0].Z);
 	}
 
 
