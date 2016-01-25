dc0a254dcd7bab09087b4dcbe7e2aa26aca5ee34
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-25 16:16:51 +0000

    Changed file paths

--- SourceCode/Ammo.cpp
+++ SourceCode/Ammo.cpp
@@ -1,7 +1,7 @@
 #include "Ammo.h"
 
 Ammo::Ammo(irr::core::vector3df spawnPosition, irr::scene::ISceneManager *sceneManagerReference, irr::video::IVideoDriver *driverReference)
-        : Collectable(spawnPosition, "Assets/Collectables/SpaceBattery.obj", "Assets/Collectables/SpaceBatteryTexture.jpg", sceneManagerReference, driverReference){
+        : Collectable(spawnPosition, "Assets/Collectables/SpaceBattery_b.obj", "Assets/Collectables/SpaceBattery_b.jpg", sceneManagerReference, driverReference){
 
     ammoIncrease = 10;
 }
