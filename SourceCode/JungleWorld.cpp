bf76b5a50047c949be4455db64b1315e9b9e1a28
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-01-27 16:20:58 +0000

    Jungle fix from windowsbuild

--- SourceCode/JungleWorld.cpp
+++ SourceCode/JungleWorld.cpp
@@ -3,5 +3,5 @@
 #include "Game.h"
 
 JungleWorld::JungleWorld(PlayerShip *player)
-	: World(player, "Assets/LevelAssets/LevelMaps/JugleWorld.stm"){
+	: World(player, "Assets/LevelAssets/LevelMaps/JungleWorld.stm"){
 }
