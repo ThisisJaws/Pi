01adc271b02fdbcc89c0db9ab07b998dfd66f9a4
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-04 18:36:04 +0000

    Set up to display something

--- SourceCode/ScoreScreen.cpp
+++ SourceCode/ScoreScreen.cpp
@@ -1,1 +1,32 @@
 #include "ScoreScreen.h"
+
+ScoreScreen::ScoreScreen(irr::gui::IGUIEnvironment *guiEnvironment){
+	guienv = guiEnvironment;
+
+	//Init all gui elements then hide them
+	finalScore = guienv->addStaticText(L"Final Score: DISPLAY TEST", irr::core::rect<irr::s32>(0, 10, 300, 40));
+	finalScore->setVisible(false);
+}
+
+void ScoreScreen::addScore(const std::string &playerName, const unsigned int &score){
+	//Add it onto the vector
+	scores.push_back(scoreData(playerName, score));
+	//Then sort the vector
+	sortVector(scores);
+}
+
+void ScoreScreen::displayScore(const bool &display){
+	finalScore->setVisible(display);
+	//instructionsText->setVisible(display);
+	//for(int i = 0; i < MAX_DISPLAY; i++){
+	//	scoreNumbers[i]->setVisible(display);
+	//}
+}
+
+void ScoreScreen::writeToFile(){
+
+}
+
+void ScoreScreen::sortVector(std::vector<scoreData> &vectorToSort){
+	
+}
--- SourceCode/ScoreScreen.h
+++ SourceCode/ScoreScreen.h
@@ -1,6 +1,15 @@
 #ifndef SCORESCREEN_H
 #define SCORESCREEN_H
 
+#include <vector>
+#include <algorithm>
+#include <string>
+
+#include "irrlicht.h"
+
+//How many names to display
+#define MAX_DISPLAY 10
+
 /*
  * Score screen class will hold all of the data for the scores and 
  * read and write any files associated with such
@@ -8,8 +17,46 @@
 
 class ScoreScreen{
 	//VARIABLES
+private:
+	//Reference to the gui environment
+	irr::gui::IGUIEnvironment *guienv;
+
+	//Structure to hold the score and name
+	struct scoreData{
+		std::string playerName;
+		unsigned int finalScore;
+
+		scoreData(const std::string &name, const unsigned int &score){
+			playerName = name;
+			finalScore = score;
+		}
+	};
+
+	//Vector of scores
+	std::vector<scoreData> scores;
+
+	//Text elements to display all of the scores
+	irr::gui::IGUIStaticText *finalScore;
+	irr::gui::IGUIStaticText *instructionsText;
+	irr::gui::IGUIStaticText *scoreNumbers[MAX_DISPLAY];
 
 	//FUNCTIONS
+public:
+	//constructor
+	ScoreScreen(irr::gui::IGUIEnvironment *guiEnvironment);
+
+	//Adds a score onto the vector
+	void addScore(const std::string &playerName, const unsigned int &score);
+
+	//Displays the score on screen
+	void displayScore(const bool &display);
+
+	//Call to write the score to file
+	void writeToFile();
+
+private:
+	//Sorts a vector from highest to lowest
+	void sortVector(std::vector<scoreData> &vectorToSort);
 };
 
 #endif
