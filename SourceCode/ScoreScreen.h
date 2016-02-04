a2f66dc8a34f2a5fc5e73ca81ccf6078bf30c78d
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-04 17:15:21 +0000

    Added score screen class

---
+++ SourceCode/ScoreScreen.h
@@ -0,0 +1,15 @@
+#ifndef SCORESCREEN_H
+#define SCORESCREEN_H
+
+/*
+ * Score screen class will hold all of the data for the scores and 
+ * read and write any files associated with such
+ */
+
+class ScoreScreen{
+	//VARIABLES
+
+	//FUNCTIONS
+};
+
+#endif
