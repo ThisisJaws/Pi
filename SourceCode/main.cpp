7aed1d981b68f7f421f50fa2e2b3bcf65a9d2852
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-19 09:31:55 +0000

    Version increase

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -21,7 +21,7 @@
 //Defines for version number
 #define CURRENT_VERSION_MAJOR	 0
 #define CURRENT_VERSION_MINOR	 6
-#define CURRENT_VERSION_REVISION 11
+#define CURRENT_VERSION_REVISION 12
 
 /*
  * program entry point
