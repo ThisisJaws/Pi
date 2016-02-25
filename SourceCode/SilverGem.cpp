734ed7b9ae513dc6f5137c72e6f8712f5b4d4689
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-25 15:25:54 +0000

    Fixed texture

--- SourceCode/SilverGem.cpp
+++ SourceCode/SilverGem.cpp
@@ -1,6 +1,6 @@
 #include "SilverGem.h"
 
 SilverGem::SilverGem(irr::core::vector3df spawnPosition, irr::scene::ISceneManager * sceneManagerReference, audiere::AudioDevicePtr audiereDevice)
-	: Gem(spawnPosition, 50000, "Assets/Collectables/SpaceGem_Bronze.jpg", sceneManagerReference, audiereDevice){
+	: Gem(spawnPosition, 50000, "Assets/Collectables/SpaceGem_Silver.jpg", sceneManagerReference, audiereDevice){
 	//Everything has been passed through
 }
