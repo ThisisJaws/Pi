b56773e62d206aae9ff12d4a049adb66b4ef282a
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-25 15:27:32 +0000

    Adjusted points

--- SourceCode/GoldGem.cpp
+++ SourceCode/GoldGem.cpp
@@ -1,6 +1,6 @@
 #include "GoldGem.h"
 
 GoldGem::GoldGem(irr::core::vector3df spawnPosition, irr::scene::ISceneManager * sceneManagerReference, audiere::AudioDevicePtr audiereDevice)
-	: Gem(spawnPosition, 100000, "Assets/Collectables/SpaceGem_Gold.jpg", sceneManagerReference, audiereDevice){
+	: Gem(spawnPosition, 50000, "Assets/Collectables/SpaceGem_Gold.jpg", sceneManagerReference, audiereDevice){
 	//Everything has been passed through
 }
