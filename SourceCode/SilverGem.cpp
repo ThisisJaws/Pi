fe3e7c239c1d3b32dc91db796a9cf21ee0e444b3
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-02-25 15:29:17 +0000

    Scores again

--- SourceCode/SilverGem.cpp
+++ SourceCode/SilverGem.cpp
@@ -1,6 +1,6 @@
 #include "SilverGem.h"
 
 SilverGem::SilverGem(irr::core::vector3df spawnPosition, irr::scene::ISceneManager * sceneManagerReference, audiere::AudioDevicePtr audiereDevice)
-	: Gem(spawnPosition, 25000, "Assets/Collectables/SpaceGem_Silver.jpg", sceneManagerReference, audiereDevice){
+	: Gem(spawnPosition, 20000, "Assets/Collectables/SpaceGem_Silver.jpg", sceneManagerReference, audiereDevice){
 	//Everything has been passed through
 }
