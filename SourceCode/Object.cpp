b34767b658147d4c3f666481e2fafeae587b1b55
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-03-02 23:41:27 +0000

    Increased collision check range

--- SourceCode/Object.cpp
+++ SourceCode/Object.cpp
@@ -116,7 +116,7 @@ irr::s32 Object::checkCollision(int direction){
 	irr::core::line3df ray;
 	ray.start = getPosition();
 	ray.end = ray.start;
-	ray.end.Z += 1 * direction;
+	ray.end.Z += direction * 2;
 	//Current interection of a level or a mesh
 	irr::core::vector3df interesection;
 	//The triangle that was hit
