6fa1a064c983a5baaed020f2319048f8e9cde292
AGarlicMonkey <wolfplayeral@yahoo.co.uk>
2016-03-08 17:45:30 +0000

    Engine compiled but the drivers do not work

---
+++ ogl-es_ToCompile/source/Irrlicht/CXMLReader.d
@@ -0,0 +1,8 @@
+CXMLReader.o: CXMLReader.cpp CXMLReader.h ../../include/IXMLReader.h \
+ ../../include/IReferenceCounted.h ../../include/irrTypes.h \
+ ../../include/IrrCompileConfig.h ../../include/irrXML.h CXMLReaderImpl.h \
+ ../../include/irrXML.h ../../include/irrString.h \
+ ../../include/irrAllocator.h ../../include/irrMath.h \
+ ../../include/irrArray.h ../../include/heapsort.h \
+ ../../include/fast_atof.h ../../include/irrString.h \
+ ../../include/IReadFile.h ../../include/coreutil.h ../../include/path.h
