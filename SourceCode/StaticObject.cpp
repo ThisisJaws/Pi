e124c4a7037bef48134a642a4b4ad03a92c4a527
agarlicmonkey <wolfplayeral@yahoo.co.uk>
2016-04-07 14:14:52 +0100

    Fixed static object rotation bug

--- SourceCode/StaticObject.cpp
+++ SourceCode/StaticObject.cpp
@@ -2,12 +2,12 @@
 
 #include "Game.h"
 
-StaticObject::StaticObject(const irr::core::vector3df &spawnPosition, const irr::io::path &pathOfMesh, const irr::io::path &pathOfTexture, irr::scene::ISceneManager *sceneManagerReference, bool updateRotation, bool checkForCollision)
+StaticObject::StaticObject(const irr::core::vector3df &spawnPosition, const irr::io::path &pathOfMesh, const irr::io::path &pathOfTexture, irr::scene::ISceneManager *sceneManagerReference, bool rotate, bool checkForCollision)
     : Object(pathOfMesh, pathOfTexture, sceneManagerReference, spawnPosition, TYPE_STATIC_OBJECT){
 
-    //set up the rotation variables
+    //Set up the rotation variables
+	this->rotate = rotate;
 	if(rotate){
-		this->rotate = rotate;
 		rotSpeed = (float)(rand() % 50 + 1);
 		//make the x y z 0 - 3 the multiply by the speed
 		rotAxis = irr::core::vector3df((rand() % 4) * rotSpeed, (rand() % 4) * rotSpeed, (rand() % 4) * rotSpeed);
